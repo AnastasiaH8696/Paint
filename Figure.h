bb6c03a16fce0d4b5f4df49f81e1ad89b52f49c8
anastasia.hamandrito <anastasia.hamandritov@finastra.com>
2021-08-24 08:54:35 +0300

    add color support

--- Figure.h
+++ Figure.h
@@ -4,14 +4,23 @@ class Figure : public CObject {
 	DECLARE_SERIAL(Figure) 
 	CPoint P1;
 	CPoint P2;
+	COLORREF brushColor;
+	COLORREF fillColor;
+	int penStyle;
+	int penSize;
 public:
-	Figure() {}
-	Figure(CPoint p1, CPoint p2) :
-		P1(p1), P2(p2){}
+	Figure();
+	Figure(CPoint p1, CPoint p2);
 	void Serialize(CArchive& ar);
 	virtual void Draw(CDC* dc) const { dc->Rectangle(P1.x, P1.y, P2.x, P2.y); };
 	CPoint getP1() const { return P1; }	
 	CPoint getP2() const { return P2; }
+	COLORREF getBrushColor() const { return brushColor; }
+	COLORREF getFillColor() const { return fillColor; }
+	int getPenStyle() const { return penStyle; }
+	int getPenSize() const { return penSize; }
+	void setBrushColor(COLORREF bc) { brushColor = bc; }
+	void setFillColor(COLORREF fc) { fillColor = fc; }
 	virtual void Redefine(CPoint p1, CPoint p2) { P1 = p1; P2 = p2; }
 	virtual bool isInside(const CPoint& P) const;
 	virtual void Shift(int dx, int dy);
